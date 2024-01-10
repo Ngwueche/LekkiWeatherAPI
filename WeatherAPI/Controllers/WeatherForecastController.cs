5eb8666ea9258a6e7cc72712701a2150842fc2ce
Ngwueche <wisdomngwueche@gmail.com>
2024-01-10 09:14:12 +0100

    Added more methods

--- WeatherAPI/Controllers/WeatherForecastController.cs
+++ WeatherAPI/Controllers/WeatherForecastController.cs
@@ -6,7 +6,7 @@ namespace WeatherAPI.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+        private static readonly List<string>Summaries = new List<string>
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
@@ -19,15 +19,51 @@ namespace WeatherAPI.Controllers
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        public IActionResult Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Ok(Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                Summary = Summaries[Random.Shared.Next(Summaries.Count)]
             })
-            .ToArray();
+            .ToArray());
         }
+        [HttpGet("GetWeatherForecastById/{id}")]
+        public IActionResult Get(int id)
+        {
+            var list = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Id = index,
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Count)]
+            }).ToArray();
+            var result = list.FirstOrDefault(x => x.Id == id);
+            if(result == null)
+            {
+                return NotFound($"Not Found with the id {id}");
+            }
+            return Ok(result);
+        }
+        [HttpPost("AddToSummary")]
+        public IActionResult Add(int id)
+        {
+            var list = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Id = index,
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Count)]
+            }).ToArray();
+            var result = list.FirstOrDefault(x => x.Id == id);
+            if(result == null)
+            {
+                return NotFound($"Not Found with the id {id}");
+            }
+            return Ok(result);
+        }
+
+
     }
 }
